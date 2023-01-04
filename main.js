6f34e7871ed88a5e195087be1d33ba9040a595c1
dolianich <69437697+dolianich@users.noreply.github.com>
2023-01-04 16:47:54 +0300

    upd

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const start = async () => {
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.body,
-            imageTargetSrc: '/assets/targets.mind'
+            imageTargetSrc: '/assets/targets.mind',
+            uiScanning: "#scanning" 
           });
       
           const {renderer, scene, camera} = mindarThree;
